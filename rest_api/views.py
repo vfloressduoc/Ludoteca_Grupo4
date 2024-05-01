0dfbb3f63c74bc9f7a8d832b1a8b7eb4f17a10cb
Claudio Muñoz B <145813519+claudmunozb@users.noreply.github.com>
2024-05-01 12:46:13 -0400

    Update views.py
    corrige error con la vista de "categorias"

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
-#importacion de librerias
-# Construye el rest
 from rest_framework.response import Response
-# Visualizacion del API rest
 from rest_framework.decorators import api_view
-#csrf es un protocolo de seguridad
 from django.views.decorators.csrf import csrf_exempt
-#le da formato JSON a los serializers
-from rest_framework.parsers import JSONParser   
-# importa libreria de codigos de respuesta
 from rest_framework import status
 
-from aplicacionweb.models import Producto
-from .serializers import ProductoSerializer
-
-from aplicacionweb.models import Categoria
-from .serializers import CategoriaSerializer
+from aplicacionweb.models import Producto, Categoria
+from .serializers import ProductoSerializer, CategoriaSerializer
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
@@ -26,21 +16,21 @@ def lista_Productos(request):
         serializer = ProductoSerializer(productos, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ProductoSerializer(data=data)
+        serializer = ProductoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+@csrf_exempt
+@api_view(['GET', 'POST'])
 def lista_Categorias(request):
     if request.method == 'GET':
         categorias = Categoria.objects.all()
         serializer = CategoriaSerializer(categorias, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CategoriaSerializer(data=data)
+        serializer = CategoriaSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
